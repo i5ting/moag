57c9cbe11a7111e148bd7867b548d864b9fa06cf
i5ting <i5ting@126.com>
2016-06-08 15:02:59 +0800

    add try catch

--- tpl/koa2-async/controllers/controller.js
+++ tpl/koa2-async/controllers/controller.js
@@ -10,17 +10,20 @@ var {{entity}} = $models.{{model}};
 
 exports.list = async (ctx, next) => {
   console.log(ctx.method + ' /{{models}} => list, query: ' + JSON.stringify(ctx.query));
+  try {
+    let {{models}} = await {{entity}}.getAllAsync();
   
-  let {{models}} = await {{entity}}.getAllAsync();
-  
-  await ctx.render('{{models}}/index', {
-    {{models}} : {{models}}
-  })
+    await ctx.render('{{models}}/index', {
+      {{models}} : {{models}}
+    })
+  } catch (err) {
+    return ctx.api_error(err);
+  }
 };
 
 exports.new = async (ctx, next) => {
   console.log(ctx.method + ' /{{models}}/new => new, query: ' + JSON.stringify(ctx.query));
-
+  
   await ctx.render('{{models}}/new', {
     {{model}} : {
       "_action" : "new"
@@ -31,77 +34,99 @@ exports.new = async (ctx, next) => {
 exports.show = async (ctx, next) => {
   console.log(ctx.method + ' /{{models}}/:id => show, query: ' + JSON.stringify(ctx.query) +
     ', params: ' + JSON.stringify(ctx.params));
-  let id = ctx.params.id;
-  let {{model}} = await {{entity}}.getByIdAsync(id);
+    
+  try {
+    let id = ctx.params.id;
+    let {{model}} = await {{entity}}.getByIdAsync(id);
   
-  console.log({{model}});
+    console.log({{model}});
   
-  await ctx.render('{{models}}/show', {
-    {{model}} : {{model}}
-  });
+    await ctx.render('{{models}}/show', {
+      {{model}} : {{model}}
+    });
+  } catch (err) {
+    return ctx.api_error(err);
+  }
 };
 
 exports.edit = async (ctx, next) => {
   console.log(ctx.method + ' /{{models}}/:id/edit => edit, query: ' + JSON.stringify(ctx.query) +
     ', params: ' + JSON.stringify(ctx.params));
 
-  let id = ctx.params.id;
+  try {
+    let id = ctx.params.id;
 
-  let {{model}} = await {{entity}}.getByIdAsync(id);
+    let {{model}} = await {{entity}}.getByIdAsync(id);
   
-  console.log({{model}});
-  {{model}}._action = 'edit';
+    console.log({{model}});
+    {{model}}._action = 'edit';
 
-  await ctx.render('{{models}}/edit', {
-    {{model}} : {{model}}
-  });
+    await ctx.render('{{models}}/edit', {
+      {{model}} : {{model}}
+    });
+  } catch (err) {
+    return ctx.api_error(err);
+  }
 };
 
 exports.create = async (ctx, next) => {
   console.log(ctx.method + ' /{{models}} => create, query: ' + JSON.stringify(ctx.query) +
     ', params: ' + JSON.stringify(ctx.params) + ', body: ' + JSON.stringify(ctx.request.body));
 
-  let {{model}} = await {{entity}}.createAsync({{keypair}});
+  try {
+    let {{model}} = await {{entity}}.createAsync({{keypair}});
   
-  console.log({{model}});
-  await ctx.render('{{models}}/show', {
-    {{model}} : {{model}}
-  });
+    console.log({{model}});
+    await ctx.render('{{models}}/show', {
+      {{model}} : {{model}}
+    });
+  } catch (err) {
+    return ctx.api_error(err);
+  }
 };
 
 exports.update = async (ctx, next) => {
   console.log(ctx.method + ' /{{models}}/:id => update, query: ' + JSON.stringify(ctx.query) +
     ', params: ' + JSON.stringify(ctx.params) + ', body: ' + JSON.stringify(ctx.request.body));
 
-  let id = ctx.params.id;
+  try {
+    let id = ctx.params.id;
 
-  let {{model}} = await {{entity}}.updateByIdAsync(id,{{keypair}});
+    let {{model}} = await {{entity}}.updateByIdAsync(id,{{keypair}});
   
-  ctx.body = ({
-    data:{
-      redirect : '/{{models}}/' + id
-    },
-    status:{
-      code : 0,
-      msg  : 'delete success!'
-    }
-  });
+    ctx.body = ({
+      data:{
+        redirect : '/{{models}}/' + id
+      },
+      status:{
+        code : 0,
+        msg  : 'delete success!'
+      }
+    });
+  } catch (err) {
+    return ctx.api_error(err);
+  }
 };
 
 exports.destroy = async (ctx, next) => {
   console.log(ctx.method + ' /{{models}}/:id => destroy, query: ' + JSON.stringify(ctx.query) +
     ', params: ' + JSON.stringify(ctx.params) + ', body: ' + JSON.stringify(ctx.request.body));
-  let id = ctx.params.id;
+    
+  try {
+    let id = ctx.params.id;
   
-  await {{entity}}.deleteByIdAsync(id);
+    await {{entity}}.deleteByIdAsync(id);
   
-  ctx.body = ({
-    data:{},
-    status:{
-      code : 0,
-      msg  : 'delete success!'
-    }
-  });
+    ctx.body = ({
+      data:{},
+      status:{
+        code : 0,
+        msg  : 'delete success!'
+      }
+    });
+  } catch (err) {
+    return ctx.api_error(err);
+  }
 };
 
 // -- custom
@@ -109,50 +134,70 @@ exports.destroy = async (ctx, next) => {
 // -- custom api
 exports.api = {
   list: async (ctx, next) => {
-    let {{model}}_id = ctx.api_{{model}}._id;
+    try {
+      let {{model}}_id = ctx.api_{{model}}._id;
 
-    let {{models}} = await {{entity}}.queryAsync({});
+      let {{models}} = await {{entity}}.queryAsync({});
     
-    await ctx.api({
-      {{models}} : {{models}}
-    })
+      await ctx.api({
+        {{models}} : {{models}}
+      })
+    } catch (err) {
+      return ctx.api_error(err);
+    }
   },
   show: async (ctx, next) => {
-    let {{model}}_id = ctx.api_{{model}}._id;
-    let id = ctx.params.{{model}}_id;
+    try {
+      let {{model}}_id = ctx.api_{{model}}._id;
+      let id = ctx.params.{{model}}_id;
 
-    let {{model}} = await {{entity}}.getByIdAsync(id);
+      let {{model}} = await {{entity}}.getByIdAsync(id);
     
-    await ctx.api({
-      {{model}} : {{model}}
-    });
+      await ctx.api({
+        {{model}} : {{model}}
+      });
+    } catch (err) {
+      return ctx.api_error(err);
+    }
   },
   create: async (ctx, next) => {
-    let {{model}}_id = ctx.api_{{model}}._id;
+    try {
+      let {{model}}_id = ctx.api_{{model}}._id;
 
-    let {{model}} = await {{entity}}.createAsync({{keypair}});
+      let {{model}} = await {{entity}}.createAsync({{keypair}});
     
-    ctx.body = ({
-      {{model}} : {{model}}
-    });
+      ctx.body = ({
+        {{model}} : {{model}}
+      });
+    } catch (err) {
+      return ctx.api_error(err);
+    }
   },
   update: async (ctx, next) => {
-    let {{model}}_id = ctx.api_{{model}}._id;
-    let id = ctx.params.{{model}}_id;
+    try {
+      let {{model}}_id = ctx.api_{{model}}._id;
+      let id = ctx.params.{{model}}_id;
     
-    let {{model}} = await {{entity}}.updateByIdAsync(id, {{keypair}});
+      let {{model}} = await {{entity}}.updateByIdAsync(id, {{keypair}});
     
-    await ctx.api({
-      {{model}} : {{model}},
-      redirect : '/{{models}}/' + id
-    });
+      await ctx.api({
+        {{model}} : {{model}},
+        redirect : '/{{models}}/' + id
+      });
+    } catch (err) {
+      return ctx.api_error(err);
+    }
   },
   delete: async (ctx, next) => {
-    let {{model}}_id = ctx.api_{{model}}._id;
-    let id = ctx.params.{{model}}_id;
+    try {
+      let {{model}}_id = ctx.api_{{model}}._id;
+      let id = ctx.params.{{model}}_id;
 
-    await {{entity}}.deleteByIdAsync(id);
+      await {{entity}}.deleteByIdAsync(id);
     
-    await ctx.api({id: id});
+      await ctx.api({id: id});
+    } catch (err) {
+      return ctx.api_error(err);
+    }
   }
 }

7ff6310e1db3899c7ddefd2a1c806a409749d392
i5ting <i5ting@126.com>
2016-06-12 04:22:47 +0800

    lazycommmit, please ignore

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ import test from 'ava';
 var exec = require('co-exec');
 
 test('exec()', function * (t) {
-    var commit = yield exec('sh c.sh');
+    var commit = yield exec('ava moa2/test -v');
     console.log(commit)
     t.true(commit == 1);
 });
